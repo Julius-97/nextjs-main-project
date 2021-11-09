98c2879bde69e9d50959d8519aafe5135e14e9ea
Julius-97 <61832549+Julius-97@users.noreply.github.com>
2021-11-09 21:44:32 +0100

    test 3

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -33,8 +33,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<outData>) => {
 
     let client: MongoClient;
 
-    //const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERNAME}.wee0h.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
-    const connectionString = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-shard-00-00.wee0h.mongodb.net:27017,cluster0-shard-00-01.wee0h.mongodb.net:27017,cluster0-shard-00-02.wee0h.mongodb.net:27017/${process.env.MONGODB_DATABASE}?ssl=true&replicaSet=atlas-c61772-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERNAME}.wee0h.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+    //const connectionString = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-shard-00-00.wee0h.mongodb.net:27017,cluster0-shard-00-01.wee0h.mongodb.net:27017,cluster0-shard-00-02.wee0h.mongodb.net:27017/${process.env.MONGODB_DATABASE}?ssl=true&replicaSet=atlas-c61772-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
     try {
       client = await MongoClient.connect(connectionString);
